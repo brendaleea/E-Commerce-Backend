89874849f7c1cf48e76e8807f7265dee91de5413
ba23 <blsarabia7@gmail.com>
2024-01-14 17:59:47 -0700

    added id,content to models,routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,18 +3,42 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll()
+  res.json(categories)
+  } catch (error) {
+  console.log (error)  
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categories= await Category.findOne ({
+      where:{
+        id:req.params.id
+      }
+    })
+    res.json(categories)
+  } catch (error) {
+   console.log (error) 
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category= await Category.create(req.body);
+    res.json(category)
+  } catch (error) {
+    console.log(error)
+    
+  }
 });
 
 router.put('/:id', (req, res) => {
